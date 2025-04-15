7ce12714bca0050f66e5158305ab87bad8e16dfe
ManuelWestermeier <westermeier111@gmail.com>
2025-04-15 10:14:58 +0200

    some fs changes

---
+++ protocoll/physikal.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Arduino.h>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+struct PhysikalConnection
+{
+    uint8_t inpPin;
+    uint8_t outPin;
+};
+
+struct PhysikalPocket
+{
+};
+
+struct PhysikalNode
+{
+    Node logicalNode;
+
+    void send(Address to, uint8_t *data)
+    {
+    }
+
+    void loop()
+    {
+    }
+
+    vector<PhysikalConnection> pins;
+};
